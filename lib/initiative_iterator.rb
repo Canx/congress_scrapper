8ca744acf8f575e68b4dab72a53e9d496fb053ce
Canx <canchete@gmail.com>
2011-03-24 17:06:47 +0100

    in the way of creating iterator

---
+++ lib/initiative_iterator.rb
@@ -0,0 +1,30 @@
+# external iterator to obtain initiative objects
+class InitiativeIterator
+
+  def initialize(page)
+     actual_page = page.search(".titulo_iniciativa a")
+  end
+
+  # check if we already are in the last page and in the last initiative
+  def has_next?
+     if 
+  end
+
+  # return initiative object
+  def item
+
+  end
+
+  # moves iterator to next initiative
+  def next_item
+
+  end
+
+  private
+
+  def next_page
+
+  end
+
+  
+end
