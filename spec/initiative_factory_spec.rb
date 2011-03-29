bbbac79cef85259b9c15bfbfdf773e3b4a5d2de8
Canx <canchete@gmail.com>
2011-03-29 22:38:00 +0200

    patched cassette and refactored test

--- spec/initiative_factory_spec.rb
+++ spec/initiative_factory_spec.rb
@@ -3,15 +3,17 @@ require File.dirname(__FILE__) + '/spec_helper'
 require File.dirname(__FILE__) + '/../lib/initiative_factory.rb'
 
 describe "InitiativeFactory" do
-  
+  before(:each) do
+      @title = "Moción de reprobación a don Gaspar Zarrías Arévalo"
+      @params = {"title" => @title}
+      @pages=PageIterator.new
+  end
+
   it "should return the initiative title" do
     VCR.use_cassette("initiative_title") do
-      title = "Moción de reprobación a don Gaspar Zarrías Arévalo"
-      params = {"title" => title}
       initiatives = []
-      pages=PageIterator.new
-      pages.each_initiative_with(params) { |i| initiatives << i }
-      initiatives.first[:title].should =~ /#{title}/
+      @pages.each_initiative_with(@params) { |i| initiatives << i }
+      initiatives.first[:title].should =~ /#{@title}/
     end
   end
 
